dfa8850818328c4ac2fbffc4c6481dfce5302606
Flash USDT 2025 <67941365+Hamtool@users.noreply.github.com>
2025-01-05 08:54:43 -0800

    Update FlashUSDTLiquidity 2025.sol

--- FlashUSDTLiquidity.sol
+++ FlashUSDTLiquidity.sol
@@ -20,6 +20,5 @@ contract Token is TRC20, TRC20Detailed {
         _mint(msg.sender, 500000000000 * (10 ** uint256(decimals())));
     }
 }
-BELOW ARE YOUR LIQUIDITY SEVER
 
-{"address":"411ff563f8800df4e5893b106e14204fe3551662dc","id":"f0b811f7-58a1-4c93-b21a-83687ed259f7","version":3,"crypto":{"cipher":"aes-128-ctr","cipherparams":{"iv":"7973ad808ba0a31964545d18e27fda50"},"ciphertext":"29803da924d8e1ca6f086896beff4e9a6debc8c8a3d15692587e2c925e71e658","kdf":"scrypt","kdfparams":{"salt":"d33dbfba3d407d9f92d686a5ca596398683ab50b1097904d2cbf66bc52818c1e","n":131072,"dklen":32,"p":1,"r":8},"mac":"3e8f8c9d60093e9ffadcacc71fca183b0fa71e9db3c7e9eddefc820dbc528b2a"}}
+
